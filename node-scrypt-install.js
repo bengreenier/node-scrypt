1fbb46f6007f9fd4f4d1fedd45fd5d036078af86
bengreenier <begreen@microsoft.com>
2016-09-07 15:37:04 -0400

    support for prebuilt scrypt

---
+++ node-scrypt-install.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var OS = require("os")
+  , fs = require('fs')
+  , path = require('path').dirname(require.main.filename)
+  , exec = require('child_process').exec
+  , puts = function(error, stdout, stderr) { if (error) console.log("Error: " + error);}
+  , hasPrebuilt = false;
+
+var platDir = __dirname + "/prebuilt/" + OS.platform() + "/" + process.arch;
+
+if (fs.existsSync(platDir)) {
+    var outDir = __dirname + "/build/Release";
+
+    try {
+        fs.mkdirSync(__dirname + "/build");
+    } catch (e) {
+        // swallow
+    }
+    try {
+        fs.mkdirSync(__dirname + "/build/Release");
+    } catch (e) {
+        // swallow
+    }
+
+    var files = fs.readdirSync(platDir);
+    for (var i = 0 ; i < files.length ; i ++) {
+        fs.createReadStream(platDir + "/" + files[i]).pipe(fs.createWriteStream(outDir + "/" + files[i]));
+    }
+
+    hasPrebuilt = true;
+    console.log("Using prebuilt " + process.arch + " binary");
+}
+
+if (!hasPrebuilt) {
+    console.log("No prebuilt binary, building...");
+    exec("node-gyp rebuild", puts);
+}
